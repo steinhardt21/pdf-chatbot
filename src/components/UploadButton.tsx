59cdecd61fd9a3b76c3f5b81309fbbb813d8905c
steinhardt21 <alexandru.enache@protonmail.com>
2024-03-19 11:16:48 +0200

    feat: created the Upload button

---
+++ src/components/UploadButton.tsx
@@ -0,0 +1,26 @@
+'use client'
+import React, { useState } from 'react'
+import { Dialog, DialogContent, DialogTrigger } from './ui/dialog'
+import { Button } from './ui/button'
+
+const UploadButton = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  return (
+    <Dialog 
+      open={isOpen} 
+      onOpenChange={(visible) => {
+        if(!visible) setIsOpen(visible)
+      }}
+    >
+      <DialogTrigger onClick={() => setIsOpen(true)} asChild>
+        <Button>Upload PDF</Button>
+      </DialogTrigger>
+      <DialogContent>
+        eaxmp
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default UploadButton
