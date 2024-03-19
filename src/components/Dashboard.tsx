bdeb7bb7533cde27a65c212bab4fd5fb0d410445
steinhardt21 <alexandru.enache@protonmail.com>
2024-03-19 21:06:02 +0200

    fix(dashboard): correct gap

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -36,7 +36,7 @@ const Dashboard = () => {
 
       {/* Display all user files */}
       {files && files.length !== 0 ? (
-        <ul className="mt-8 grid grid-cols-1 grap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3">
+        <ul className="mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3">
           {files.sort(
             (a, b) => 
               new Date(b.createdAt).getTime() -
