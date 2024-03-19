82590d011b3c2fc5ffc2843b82445bc1ecf8b822
steinhardt21 <alexandru.enache@protonmail.com>
2024-03-19 11:15:05 +0200

    feat(trpc/server): created the protected route

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,7 +1,26 @@
-import { initTRPC } from "@trpc/server"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { TRPCError, initTRPC } from "@trpc/server"
 
 // Initialization of tRPC backend
 const t = initTRPC.create()
+const middleware = t.middleware
+
+const isAuth = middleware(async (options) => {
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
+
+  if(!user || !user.id) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' })
+  }
+
+  return options.next({
+    ctx: {
+      userId: user.id,
+      user
+    }
+  })
+})
 
 export const router = t.router
 export const publicProcedure = t.procedure
+export const privateProcedure = t.procedure.use(isAuth) // when this procedure is called, the user must be authenticated
