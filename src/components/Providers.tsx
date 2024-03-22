2bdfdbc5f282b7054e37655ef05fc10e81d57043
steinhardt21 <alexandru.enache@protonmail.com>
2024-03-22 22:00:39 +0200

    chore: changed localhost

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -11,7 +11,7 @@ const Providers = ({ children }: PropsWithChildren) => {
   const [trpcClient] = useState(() => trpc.createClient({
     links: [
       httpBatchLink({
-        url: 'http://localhost:3000/api/trpc'
+        url: 'http://localhost:3009/api/trpc'
       })
     ]
   }))
