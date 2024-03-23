cb988040fda40300322de1496b5154dbc72a9a83
steinhardt21 <alexandru.enache@protonmail.com>
2024-03-23 19:34:54 +0200

    feat: added the connection to Pinecone and the upload process of the pdf

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,7 +1,12 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf"
+import { OpenAIEmbeddings } from "langchain/embeddings/openai"
+import { PineconeStore } from "langchain/vectorstores/pinecone"
+
 
 import { db } from "@/db";
+import { getPineconeClient  } from "@/lib/pinecone";
 
 const f = createUploadthing();
   
@@ -29,6 +34,47 @@ export const ourFileRouter = {
           uploadStatus: "PROCESSING"
         }
       })
+
+      try {
+        const response = await fetch(file.url)
+        const blob = await response.blob()
+
+        const loader = new PDFLoader(blob)
+
+        const pageLevelDocs = await loader.load()
+        const pagesAsmt = pageLevelDocs.length
+
+        //vectorize and index entire document
+        const pinecone = await getPineconeClient()
+        const pineconeIndex = pinecone.Index('chat-pdf')
+        const embeddings = new OpenAIEmbeddings({ 
+          openAIApiKey: process.env.OPENAI_API_KEY
+        })
+
+        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+          pineconeIndex,
+          namespace: createFile.id
+        })
+
+        await db.file.update({
+          data: {
+            uploadStatus: "SUCCESS"
+          },
+          where: {
+            id: createFile.id
+          }
+        })
+
+      } catch(error) {
+        await db.file.update({
+          data: {
+            uploadStatus: "FAILED"
+          },
+          where: {
+            id: createFile.id
+          }
+        })
+      }
     }),
 } satisfies FileRouter;
  
